326e9f12f6a1aa4a974651c2d55e8f8ebaaec2f9
Nikhith Tummalapalli <nikhith@protonmail.com>
2017-11-13 11:57:01 +0530

    Add files via upload

---
+++ online_voting/student.php
@@ -0,0 +1,23 @@
+<?php
+require('connection.php');
+
+session_start();
+//If your session isn't valid, it returns you to the login screen for protection
+if(empty($_SESSION['member_id'])){
+ header("location:access-denied.php");
+}
+?>
+<html><head>
+<link href="css/user_styles.css" rel="stylesheet" type="text/css" />
+</head><body bgcolor="tan">
+
+<center><b><font color = "brown" size="6">Simple PHP Polling System</font></b></center><br><br>
+<div id="page">
+<div id="header">
+<h1>STUDENT HOME </h1>
+<a href="student.php">Home</a> | <a href="vote.php">Current Polls</a> | <a href="manage-profile.php">Manage My Profile</a> | <a href="logout.php">Logout</a>
+</div>
+
+
+</div>
+</body></html>
